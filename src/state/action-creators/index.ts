64e9e2ba19e8b272d79755ec2f597076366efde8
yeonlisa <tkzw4869@gmail.com>
2022-02-22 16:00:16 +0900

    Typing Dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -20,7 +20,7 @@ export const withdrawMoney = (amount: number) => {
     }
 }
 
-export const bankrupt = (amount: number) => {
+export const bankrupt = () => {
     return (dispatch: Dispatch<Action>) => {
         dispatch({
             type: ActionType.BANKRUPT
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -7,4 +7,6 @@ const reducers = combineReducers({
     bank: bankReducer
 });
 
-export default reducers
+export default reducers;
+
+export type State = ReturnType<typeof reducers>
