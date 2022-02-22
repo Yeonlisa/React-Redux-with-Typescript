8efec705b38e6a4564d1b033c78bc35362c7b009
yeonlisa <tkzw4869@gmail.com>
2022-02-22 15:34:23 +0900

    Organizing Files

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+import { combineReducers } from 'redux';
+import bankReducer from './bankReducer';
+
+
+const reducers = combineReducers({
+    bank: bankReducer
+});
+
+export default reducers
